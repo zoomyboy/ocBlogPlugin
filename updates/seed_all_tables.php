9b0bfe6c2ac5a063486c3b92dd31a5ec330bad97
Philipp Lang <pille@stammsilva.de>
2017-01-09 21:23:01 +0100

    Disabled default seeding

--- updates/seed_all_tables.php
+++ updates/seed_all_tables.php
@@ -10,25 +10,6 @@ class SeedAllTables extends Seeder
 
     public function run()
     {
-        Post::create([
-            'title' => 'First blog post',
-            'slug' => 'first-blog-post',
-            'content' => '
-This is your first ever **blog post**! It might be a good idea to update this post with some more relevant content.
-
-You can edit this content by selecting **Blog** from the administration back-end menu.
-
-*Enjoy the good times!*
-            ',
-            'excerpt' => 'The first ever blog post is here. It might be a good idea to update this post with some more relevant content.',
-            'published_at' => Carbon::now(),
-            'published' => true
-        ]);
-
-        Category::create([
-            'name' => trans('rainlab.blog::lang.categories.uncategorized'),
-            'slug' => 'uncategorized',
-        ]);
     }
 
 }
